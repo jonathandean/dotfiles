fd91d5c253fcd556971eb967203b55aa1c15a753
Jonathan Dean <jon@jonathandean.com>
2020-06-02 23:03:16 -0400

    Create jon-custom-aliases.plugin.zsh

---
+++ .oh-my-zsh/custom/plugins/jon-custom-aliases/jon-custom-aliases.plugin.zsh
@@ -0,0 +1,18 @@
+# Get to common directories fast
+alias proj='cd ~/Projects'
+
+# Rails
+alias fs='bundle exec foreman start'
+alias rspecs="RAILS_ENV=test RACK_ENV=test bundle exec rake spec"
+alias rspecsff="RAILS_ENV=test RACK_ENV=test bundle exec rspec spec/ --fail-fast"
+alias runspec="RAILS_ENV=test RACK_ENV=test bundle exec rspec"
+alias resetdb='bundle exec rake db:drop db:create db:migrate db:seed'
+alias migrate='be rake db:migrate && RAILS_ENV=test be rake db:migrate'
+
+# Git
+alias pull="git pull --rebase"
+alias master="git checkout master && git pull --rebase && bundle install"
+
+# Network throttle
+alias slowweb='sudo ipfw pipe 1 config bw 15KByte/s'
+alias fastweb='sudo ipfw delete 1'
