68a94597a306af297dd10ead3087f6464416ee99
Jonathan Dean <jon@jonathandean.com>
2021-04-26 18:43:05 -0400

    Use bin for rails and rake, main instead of master branches, etc

--- .oh-my-zsh/custom/plugins/jon-custom-aliases/jon-custom-aliases.plugin.zsh
+++ .oh-my-zsh/custom/plugins/jon-custom-aliases/jon-custom-aliases.plugin.zsh
@@ -7,13 +7,17 @@ alias fsd='bundle exec foreman start -f Procfile.dev'
 alias rspecs="RAILS_ENV=test RACK_ENV=test bundle exec rake spec"
 alias rspecsff="RAILS_ENV=test RACK_ENV=test bundle exec rspec spec/ --fail-fast"
 alias runspec="RAILS_ENV=test RACK_ENV=test bundle exec rspec"
-alias resetdb='bundle exec rake db:drop db:create db:migrate db:seed'
-alias migrate='be rake db:migrate && RAILS_ENV=test be rake db:migrate'
-alias rollback='be rake db:rollback && RAILS_ENV=test be rake db:rollback'
+alias resetdb='bin/rake db:drop db:create db:migrate db:seed'
+alias migrate='bin/rake db:migrate && RAILS_ENV=test be rake db:migrate'
+alias rollback='bin/rake db:rollback && RAILS_ENV=test be rake db:rollback'
+alias rc='bin/rails c'
+alias rk="bin/rake"
+alias routes="bin/rails routes"
 
 # Git
 alias pull="git pull --rebase"
 alias master="git checkout master && git pull --rebase && bundle install"
+alias main="git checkout main && git pull --rebase && bundle install"
 
 # Network throttle
 alias slowweb='sudo ipfw pipe 1 config bw 15KByte/s'
